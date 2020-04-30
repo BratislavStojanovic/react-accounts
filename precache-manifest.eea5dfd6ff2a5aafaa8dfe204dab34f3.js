21c5bfba57071b0243676678bb71e686c4d1b71d
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2020-04-30 19:45:01 +0200

    Updates

---
+++ precache-manifest.eea5dfd6ff2a5aafaa8dfe204dab34f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3756faacb68c62a63f7136e4dfa147a7",
+    "url": "/react-accounts/index.html"
+  },
+  {
+    "revision": "7a88c812edfdfcda9451",
+    "url": "/react-accounts/static/css/main.8c3ca874.chunk.css"
+  },
+  {
+    "revision": "27314730dc79f0aca4a0",
+    "url": "/react-accounts/static/js/2.bc91b278.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-accounts/static/js/2.bc91b278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a88c812edfdfcda9451",
+    "url": "/react-accounts/static/js/main.57686fbf.chunk.js"
+  },
+  {
+    "revision": "2c53f3ddf67f8341fd30",
+    "url": "/react-accounts/static/js/runtime-main.445f74fc.js"
+  }
+]);
